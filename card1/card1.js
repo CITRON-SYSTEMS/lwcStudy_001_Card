b12d163fe65244381d4674ffe6ac872cf03ea2e9
koji_udagawa <koji.udagawa@citron-reiwa.com>
2022-04-27 23:50:16 +0900

    init

---
+++ card1/card1.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class card1 extends LightningElement {}
---
+++ card3/card3.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class card3 extends LightningElement {}
